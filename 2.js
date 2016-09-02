c9326300a4f8e35af33dfe7f1f4441f6286ae83a
570295535 <570295535@qq.com>
2016-09-02 11:14:36 +0800

    Update 2.js

--- 2.js
+++ 2.js
@@ -41,14 +41,3 @@ recognition.onerror = function(event){
   console.log('error?');
   console.log(event);
 }
-
-
-function mm()
-{
-    var e; if(e=document.getElementById("meizz")) e.parentNode.removeChild(e);
-    var script= document.createElement("SCRIPT"); script.defer = true;
-    script.type="text/javascript"; script.id="meizz";
-    script.src = "./2.js?temp="+ Math.random();  //缓存问题
-    document.getElementsByTagName("HEAD")[0].appendChild(script);
-}
-setInterval("mm()", 30000); //30秒运行一次
