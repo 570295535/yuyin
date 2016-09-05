bdeac7e6af435fc967ac9a25a7273532d467e300
570295535 <570295535@qq.com>
2016-09-05 12:47:38 +0800

    Update 3.js

--- 3.js
+++ 3.js
@@ -3,7 +3,7 @@ function mm()
     var e; if(e=document.getElementById("meizz")) e.parentNode.removeChild(e);
     var script= document.createElement("SCRIPT"); script.defer = true;
     script.type="text/javascript"; script.id="meizz";
-    script.src = "https://raw.githubusercontent.com/570295535/yuyin/master/2.js?temp="+ Math.random();  //缓存问题
-    document.getElementsByTagName("HEAD")[0].appendChild(script);
+    script.src = "./2.js?temp="+ Math.random();  //缓存问题
+    document.getElementsByTagName("body")[0].appendChild(script);
 }
-setInterval("mm()", 10000); //30秒运行一次
+setInterval("mm()", 16000); //16秒运行一次
